a1841d8674efb67b1d5b50b020f228e291ac1707
Dylan Bishop <dbishoponline@gmail.com>
2017-04-04 07:47:28 -0700

    adds tests for getUserArgs(), cleanCommas(), readFiles()

--- test/cli.test.js
+++ test/cli.test.js
@@ -1,6 +1,8 @@
 import test from 'ava'
+import CLI from '../dist/cli'
 
 import { getUserArgs, cleanCommas, readFiles, countOccurs, sortByRank, getSpacing, getLongestArgWidth } from '../src/helpers'
+
 
 test(`readFiles() should return null if "files" argument is empty string`, t => {
   let res = readFiles("", () => {}, () => {}, () => {})
@@ -9,14 +11,46 @@ test(`readFiles() should return null if "files" argument is empty string`, t => 
 })
 
 test(`readFiles() should return  an array of file paths`, t => {
-  let res = readFiles('../data/*.json', (files, file, data, onComplete) => {
-    return files
-  }, () => {}, () => {})
-  t.is(res.length, 4)
+  let count = null
+
+  let res = readFiles(__dirname+'/../data/*.json', (files, file, data, onComplete) => {
+    count = files
+    onComplete(files, file)
+  }, t.fail, onDone)
+
+  function onDone(files, file){
+    count = files.length--
+    if(!count){
+      t.pass()
+    }
+  }
 })
 
-// test('bar', async t => {
-// 	const bar = Promise.resolve('bar');
-//
-// 	t.is(await bar, 'bar');
-// });
+test(`getUserArgs() should return an array of args when they exist in the user input`, t => {
+  process.argv = [ '/usr/local/Cellar/node/7.5.0/bin/node',
+  '/Users/username/dev/apps/node-exam/dist/index.js',
+  'ipsum',
+  'chicken' ]
+
+  if(getUserArgs().toString() === (process.argv).slice(2).toString()){
+    t.pass()
+  }
+  else {
+    t.fail()
+  }
+})
+
+test(`cleanCommas() should return an array without commas in any of the index values`, t => {
+  let arr = ['ipsum,', 'lorem,', 'xteam', 'development,']
+
+  let newArray = cleanCommas(arr)
+
+  newArray.forEach((item) => {
+    if(item.indexOf(',') > -1){
+      t.fail()
+      return
+    }
+  })
+
+  t.pass()
+})
